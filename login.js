f001f3867618f7cd1e5dfa518541ca595e8a8eb8
Kyle Mistele <kyle@mistele.com>
2022-01-19 13:09:54 -0600

    finish writing scripts for ironwood

--- login.js
+++ login.js
@@ -1,6 +1,13 @@
 const { ethers } = require('ethers');
 const {default: axios} = require("axios");
+require('dotenv').config();
 const API_URL = 'https://dev.zelus.io' // this can be changed to api.zelus.io if you want
+
+/** Make sure that the necessary environment variables are there */
+function checkEnvironment() {
+    if (!process.env.ADDRESS) throw new Error(`Missing address: make sure to run 'npm run register' first!`);
+    if (!process.env.PRIVATE_KEY) throw new Error(`Missing private key: make sure to run 'npm run register' first!`);
+}
 
 /**
  * Get the server time of {@link API_URL}
@@ -14,6 +21,51 @@ async function getCurrentTime() {
     return response.data.current_time
 }
 
-async function signData(data) {
 
+async function buildRequest(timestamp) {
+
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY);
+    const signature = await wallet.signMessage(timestamp);
+
+    return {
+        ethereum_address: process.env.ADDRESS.toLowerCase(),
+        timestamp: timestamp,
+        signature: signature
+    }
+}
+
+async function main() {
+
+    checkEnvironment();
+    console.log(`[INFO] environment check passed`)
+
+    // get the timestamp to sign
+    const timestamp = await getCurrentTime();
+    console.log(`[INFO] got server time from API: ${timestamp}`)
+
+    // build the request
+    const request = await buildRequest(timestamp);
+    console.log(`[INFO] built request data: ${JSON.stringify(request)}`)
+
+    try {
+        const loginResponse = await axios.post(`${API_URL}/auth/login`, request);
+        console.log(`[INFO] got status code ${loginResponse.status} from the request`);
+        if(loginResponse.data.token) {
+            console.log(`[INFO] Got token from login request: ${loginResponse.data.token}`)
+        }
+    }
+    catch (err) {
+        console.log(`[ERROR]: ${err.message}`)
+    }
+}
+
+/** Launch the code if this is run as the main module */
+if (require.main === module) {
+    main()
+        .then(() => process.exit(0))
+        .catch(err => {
+            console.error(`[ERROR] ${err.message}`);
+            console.error(err);
+            process.exit(1);
+        })
 }
